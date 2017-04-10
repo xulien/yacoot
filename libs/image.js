18d4b25a4937aca0b288ef8c333fba605139989b
julien <julien@brunet.io>
2017-04-10 10:18:32 +0200

    remove knox.js and ugly hack to use aws sdk instead

--- libs/image.js
+++ libs/image.js
@@ -3,6 +3,8 @@ var resize = require('./resize');
 var debug = require('debug')('yacoot');
 var mozjpeg = require('mozjpeg-stream');
 var write = require('./write');
+var fs = require('fs');
+var AWS = require('aws-sdk');
 
 module.exports = Image;
 
@@ -23,8 +25,42 @@ function Image(srcStream, report, outputs, cb) {
         }))
         .pipe(write(output))
         .on('close', function() {
-          debug('%s done', output.name);
-          done();
+          if (output.s3) {
+            var options = {
+              accessKeyId: output.s3.key,
+              secretAccessKey: output.s3.secret,
+              region: output.s3.region || 'eu-central-1',
+              params: {
+                Bucket: 'i2m',
+                ACL: 'public-read',
+                CacheControl: 'max-age=86400'
+              }
+            }
+
+            var fileStream = fs.createReadStream('/tmp/' + output.name + '.jpg');
+              fileStream.on('error', function (err) {
+                if (err) { throw err; }
+            });
+
+            var s3 = new AWS.S3(options);
+
+            var params = {
+              Key: output.target + '/' + output.name + '.jpg',
+              Body: fileStream
+            }
+
+            s3.putObject(params, function(err, data) {
+              if (err) console.log('Error uploading data: ', err);
+              else console.log('Successfully uploaded data: ', data);
+
+              debug('%s done', output.name);
+              done();
+            })
+
+          } else {
+            debug('%s done', output.name);
+            done();
+          }
         });
     },
     function(err) {
